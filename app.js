0d68c1b8e8c047b0e145cb3d6334388a66ccbd0c
Vinit Jain <jain.vinit14@gmail.com>
2025-02-08 22:23:07 -0500

    added separate folders for routes and controllers

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const tasks = require("./routes/tasks");
 
 const app = express();
 const port = 3000;
 
-app.get("/api/v1/tasks", (req, res) => {});
-app.get("/api/v1/tasks", (req, res) => {});
-app.get("/api/v1/tasks/:id", (req, res) => {});
-app.patch("/api/v1/tasks/:id", (req, res) => {});
-app.delete("/api/v1/tasks", (req, res) => {});
+//middleware
+app.use(express.json());
+
+//routes
+app.use("/api/v1/tasks", tasks);
 
 app.listen(port, console.log("server is listening on " + port));
