4105d0dd6c49d589bea5c5087a4c3fb6dfd5564c
Vinit Jain <jain.vinit14@gmail.com>
2025-02-08 23:39:19 -0500

    added all the controllers and routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,25 @@ const getAllTasks = (req, res) => {
   res.send("all items from the file");
 };
 
+const createTask = (req, res) => {
+  res.send("createTast");
+};
+
+const getTask = (req, res) => {
+  res.send("getTask");
+};
+
+const updateTask = (req, res) => {
+  res.send("updateTask");
+};
+const deleteTask = (req, res) => {
+  res.send("deleteTasktask");
+};
+
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
