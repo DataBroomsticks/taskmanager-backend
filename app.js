56001b3933e70d8db4aa7f753be15e5498056ea0
Vinit Jain <jain.vinit14@gmail.com>
2025-02-09 18:44:24 -0500

    setup mongodb connection

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
@@ -10,4 +12,11 @@ app.use(express.json());
 //routes
 app.use("/api/v1/tasks", tasks);
 
-app.listen(port, console.log("server is listening on " + port));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log("server is listening on " + port));
+  } catch (error) {}
+};
+
+start();
