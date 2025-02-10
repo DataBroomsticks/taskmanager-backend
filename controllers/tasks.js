c31d38fd83c4d33e569110765b58bcf27be51df6
Vinit Jain <jain.vinit14@gmail.com>
2025-02-09 22:52:52 -0500

    added 2 more routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,7 +2,7 @@ const tasks = require("../models/tasks");
 
 const getAllTasks = async (req, res) => {
   const results = await tasks.find({});
-  res.status(200).json(results);
+  res.status(200).json({ results });
 };
 
 const createTask = async (req, res) => {
@@ -15,15 +15,28 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.send("getTask");
+const getTask = async (req, res) => {
+  try {
+    const results = await tasks.findOne({ _id: req.params.id });
+    if (!results) return res.status(404).json({ msg: "didn't find anything" });
+    res.status(200).json({ results });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("updateTask");
 };
-const deleteTask = (req, res) => {
-  res.send("deleteTasktask");
+const deleteTask = async (req, res) => {
+  try {
+    const results = await tasks.deleteOne({ _id: req.params.id });
+    if (results.deletedCount === 0)
+      return res.status(404).json({ msg: "no task with that id" });
+    res.status(200).json({ results });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 module.exports = {
